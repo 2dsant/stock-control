5b79e84dc097c8eeeaa36755a4e512b0b8f6a2b1
David Santos <2dsant3@gmail.com>
2024-06-09 22:54:31 -0300

    viewchild estudo

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnDestroy } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnDestroy, ViewChild } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
@@ -13,7 +13,10 @@ import { UserService } from 'src/app/services/user/user.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnDestroy{
+export class HomeComponent implements OnDestroy, AfterViewInit{
+  @ViewChild('emailInput') public emailInputRef!: ElementRef;
+  @ViewChild('passwordInput') public passwordInputRef!: ElementRef;
+
   private destroy$ = new Subject<void>();
   loginCard = true;
 
@@ -35,6 +38,11 @@ export class HomeComponent implements OnDestroy{
     private messageService: MessageService,
     private router: Router
   ) { }
+
+  ngAfterViewInit(): void {
+    this.emailInputRef.nativeElement.value = 'Seu email aqui';
+    this.passwordInputRef.nativeElement.value = 'Sua senha aqui';
+  }
 
   onSubmitLoginForm(): void {
     this.userService.authUser(this.loginForm.value as AuthRequest)
